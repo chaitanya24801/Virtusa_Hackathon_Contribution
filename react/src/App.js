07eba4878f6876870c376c500f01a8b63d8e208b
KoppulaLikhitha <62035470+KoppulaLikhitha@users.noreply.github.com>
2021-03-30 00:12:39 +0530

    Likhitha updated app.js

--- react/src/App.js
+++ react/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./components/Main";
+import Home from "./components/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.css";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/home" component={() => <Home />} />
+          <Route exact path="/" component={() => <Main />} />
+          <Redirect to="" />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
